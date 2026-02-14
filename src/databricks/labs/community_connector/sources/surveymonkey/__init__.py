a2c3f7416ed1459a40c8d0e0b94c0ad2bfb56242
Yong Li <yong.li@databricks.com>
2026-02-14 18:27:45 +0000

    lint

--- src/databricks/labs/community_connector/sources/surveymonkey/__init__.py
+++ src/databricks/labs/community_connector/sources/surveymonkey/__init__.py
@@ -1,5 +1,7 @@
 """SurveyMonkey source connector."""
 
-from databricks.labs.community_connector.sources.surveymonkey.surveymonkey import SurveymonkeyLakeflowConnect
+from databricks.labs.community_connector.sources.surveymonkey.surveymonkey import (
+    SurveymonkeyLakeflowConnect,
+)
 
 __all__ = ["SurveymonkeyLakeflowConnect"]
