cfbf51a8baac6d12fddd5d8c7cdfee0df52324ec
dgalluzzo26 <gallod5@gmail.com>
2026-03-04 16:57:54 -0600

    feat(google_sheets_docs): step 3 - connector implementation (spreadsheets, sheet_values, documents)

---
+++ src/databricks/labs/community_connector/sources/google_sheets_docs/__init__.py
@@ -0,0 +1,7 @@
+"""Google Sheets and Google Docs source connector."""
+
+from databricks.labs.community_connector.sources.google_sheets_docs.google_sheets_docs import (
+    GoogleSheetsDocsLakeflowConnect,
+)
+
+__all__ = ["GoogleSheetsDocsLakeflowConnect"]
